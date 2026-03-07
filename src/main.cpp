6d07af21555d4e21181935c97ecfcc82b62eaecb
Corey-Burdick <coreyburdickwork@gmail.com>
2026-03-07 18:34:46 -0500

    main.cpp now creates a Game object called "game" and calls update and draw methods on that object every frame.

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,17 @@ int main() {
   
   InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Space Invaders");
   SetTargetFPS(TARGET_FPS);
+
+  Game game;
   
   while (!WindowShouldClose()) {
     BeginDrawing();
 
     ClearBackground(COLOR_BACKGROUND_GREY);
+
+    game.update();
+
+    game.draw();
 
     EndDrawing();
   }
