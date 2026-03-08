9b0c798026e7190273b239f27a7d0d8faa136faa
Bepis <coreyburdickwork@gmail.com>
2026-03-08 00:11:33 -0500

    Added windows flags.

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,15 @@
 
 int main() {
   
+  SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+  SetConfigFlags(FLAG_MSAA_4X_HINT);
+
   InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Space Invaders");
+  
   SetTargetFPS(TARGET_FPS);
+  SetWindowMinSize(1280, 720);
   SetWindowSize(GetMonitorWidth(0), GetMonitorHeight(0));
-  ToggleFullscreen();
+  //ToggleFullscreen();
 
   Game game;
   
