3510f7a97c01529896aa02a22f354228f356a921
Bepis <coreyburdickwork@gmail.com>
2026-03-11 00:15:12 -0400

    Made temp fix permanent.

--- src/main.cpp
+++ src/main.cpp
@@ -4,15 +4,12 @@
 
 int main() {
   
-  //SetConfigFlags(FLAG_WINDOW_RESIZABLE);
   SetConfigFlags(FLAG_MSAA_4X_HINT);
 
   InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Space Invaders");
   
   SetTargetFPS(TARGET_FPS);
-  SetWindowMinSize(1280, 720);
   SetWindowSize(GetMonitorWidth(0), GetMonitorHeight(0));
-  //ToggleFullscreen();
 
   Game game;
   
