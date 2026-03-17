f4697473efb8d0c8ec1ab7b6d9b48856e466c54c
Corey-Burdick <coreyburdickwork@gmail.com>
2026-03-17 15:20:30 -0400

    Cleaned up some code to make it more readable.

--- src/gamewindow.cpp
+++ src/gamewindow.cpp
@@ -5,26 +5,24 @@ GameWindow::GameWindow() {
 }
 
 GameWindow::~GameWindow() {
-
 }
 
 void GameWindow::update() {
   _updateWindowFrame();
-
 }
 
 void GameWindow::draw() {
   _drawWindowFrame();
-
 }
 
 void GameWindow::_drawWindowFrame() {
   DrawRectangleLinesEx(_gameWindowFrame, 3, WHITE);
-
 }
 
 void GameWindow::_updateWindowFrame() {
-  _gameWindowFrame =  {float(GetScreenWidth() / 2 - WINDOW_WIDTH / 2), float(GetScreenHeight() / 2 - WINDOW_HEIGHT / 2), WINDOW_WIDTH, WINDOW_HEIGHT};  
+  float posX = GetScreenWidth() / 2 - WINDOW_WIDTH / 2;
+  float posY = GetScreenHeight() / 2 - WINDOW_HEIGHT / 2;
+  _gameWindowFrame =  {posX, posY, WINDOW_WIDTH, WINDOW_HEIGHT};  
 }
 
 Vector2 GameWindow::getFrameOffset() {
