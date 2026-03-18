ffd9e28a141dc810adaae8222a93a05e792c3c2c
Bepis <coreyburdickwork@gmail.com>
2026-03-18 11:46:38 -0400

    Added some enemy boilerplate.

--- include/enemy.h
+++ include/enemy.h
@@ -4,7 +4,19 @@
 
 class Enemy {
 private:
+  Vector2 _worldPosition;  
+
+  void _getScreenPosition();
 
 public:
+  static Texture2D enemyImage[3];
+  int type;
+  Vector2 position;
+
+  Enemy(int type, Vector2 position);
+  static void UnloadImages();
+  void update();
+  void draw();
+  int GetType();
 
 };
--- src/enemy.cpp
+++ src/enemy.cpp
@@ -1,1 +1,8 @@
 #include <enemy.h>
+
+Texture2D Enemy::enemyImage[3] = {};
+
+Enemy::Enemy(int type, Vector2 position) {
+  this -> type = type;
+  _worldPosition = position;
+}
